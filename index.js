129680abc51fb7860add72a1f7bb051c1171eeec
Dmytro Tkachuk <tkachukdimon@gmail.com>
2020-05-14 21:39:37 +0300

    hw-11 timer

---
+++ index.js
@@ -0,0 +1,49 @@
+
+import buildCountdownTimer from './teplate.js'
+
+
+class CountdownTimer{
+
+  constructor({selector, targetDate}){
+    this.selector = selector
+    this.targetDate = targetDate
+  }
+
+  start(){
+    buildCountdownTimer(this.selector)
+    this.days = document.querySelector('[data-value="days"]'),
+    this.hours = document.querySelector('[data-value="hours"]'),
+    this.mins = document.querySelector('[data-value="mins"]'),
+    this.secs = document.querySelector('[data-value="secs"]')
+    this.timerId = setInterval(() => {
+      const currentTime = Date.now()
+      const time = this.targetDate - currentTime
+      this.counting(time)
+     }, 1000);
+  }
+
+  counting (time){
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    this.updateCountdown(days,hours,mins,secs)
+  }
+
+  updateCountdown (days,hours,mins,secs){
+    this.days.textContent = days
+    this.hours.textContent = String(hours).padStart(2, '0')
+    this.mins.textContent = String(mins).padStart(2, '0')
+    this.secs.textContent = String(secs).padStart(2, '0')
+  }
+
+}
+
+
+ const myCountdown = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date (2020, 6, 11,15,0,0).getTime(),
+  });
+
+
+document.addEventListener('DOMContentLoaded', myCountdown.start())
---
+++ teplate.js
@@ -0,0 +1,30 @@
+
+  export default
+
+  function buildCountdownTimer (id){
+    const markup = `
+    <div class="timer" id="${id}">
+    <div class="field">
+      <span class="value" data-value="days">11</span>
+      <span class="label">Days</span>
+    </div>
+    
+    <div class="field">
+      <span class="value" data-value="hours">11</span>
+      <span class="label">Hours</span>
+    </div>
+    
+    <div class="field">
+      <span class="value" data-value="mins">11</span>
+      <span class="label">Minutes</span>
+    </div>
+    
+    <div class="field">
+      <span class="value" data-value="secs">11</span>
+      <span class="label">Seconds</span>
+    </div>
+    </div>
+    `
+    document.body.insertAdjacentHTML ("afterbegin", markup)
+    
+    }
